7950d6637f8a1596a0bcaf58d4018186a32777d3
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-07-07 14:06:49 -0400

    configure full stack javascript part 2

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['babel-polyfill', './lib/components/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
