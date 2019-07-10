4b29f91f1e17647d92bcafbb8a7dd40851a7759e
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-07-09 20:24:02 -0400

    Using different babel configurations

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,13 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader',
+        use:{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react','@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        },
         exclude: /node_modules/
       }
     ]
